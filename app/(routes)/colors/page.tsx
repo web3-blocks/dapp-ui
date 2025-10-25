07503b146ef10958b76e527a540e45a3c5220fc2
Holiday <thelastofinusa@gmail.com>
2025-10-25 16:27:10 +0100

    feat: tailwindcss colors page

---
+++ app/(routes)/colors/page.tsx
@@ -0,0 +1,9 @@
+import { ColorSection } from "./_components/color-section";
+
+export default function ColorsPage() {
+  return (
+    <section className="relative overflow-x-clip border-b border-dashed">
+      <ColorSection />
+    </section>
+  );
+}
